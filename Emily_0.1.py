fe80b194a7d416abc41c2fe9dc20225bf04c648f
KazuhikoRui <70285464+KazuhikoRui@users.noreply.github.com>
2022-02-24 10:29:13 +0700

    Update Emily_0.1.py

--- Emily_0.1.py
+++ Emily_0.1.py
@@ -31,7 +31,7 @@ def unlog():
 	
 def clearing(sub_client):
 	i = 0
-	#global chats
+	global chats
 	print(chats)
 	for chat in chats:
 		msgs=sub_client.get_chat_messages(chatId=chat, size = 30)
@@ -42,6 +42,14 @@ def clearing(sub_client):
 			else: 
 				pass
 		return i
+
+def loadfrom(sub_client, chat):
+	ans = ''
+	msgs=sub_client.get_chat_messages(chatId=chat, size = 30)
+	for msgC, msgT, msgCon, msgA in zip(msgs.type, msgs.messageId, msgs.content, msgs.author.userId):
+		ans = ans + msgCon + ' ' + msgC + '\n'
+		return ans
+	
 
 def autoclean():
 	sub_client = log(mail, passw)
@@ -73,6 +81,18 @@ async def clear(message: types.Message):
 @dp.message_handler(commands=['счетчик'])
 async def chet(message: types.Message):
 	await message.answer(f"Проверок было: {times}")
+	
+@dp.message_handler(commands=['загрузить'])
+async def loaderfrom(message: types.Message):
+	a = await message.answer(f"Из какого чата вывести сообщения?")
+	if a == 'Рп':
+		await message.answer(loadfrom(sub_client, os.environ.get('RP')))
+	elif a == 'Флуд':
+		await message.answer(loadfrom(sub_client, os.environ.get('Flood')))
+	elif a == 'Анкета':
+		await message.answer(loadfrom(sub_client, os.environ.get('Ank')))
+	else:
+		await message.answer('Ошибка ввода')
 	
 def start_schedule_():
     schedule.every(30).minutes.do(autoclean)
